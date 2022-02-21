566e09d12cdfdd7169b0c962e886d0413714acbb
Gene Hackerman <arias.rafael.1984@gmail.com>
2022-02-21 12:14:08 -0500

    added PORT to push to port 80

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const { animals } = require('./data/animals');
 
 
 const express = require('express');
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 // filters array by category via filteredResults
@@ -59,6 +60,6 @@ app.get('/api/animals', (req, res) => {
 
 // will listen for which port
 // send will display a message on the front-end aka client
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
